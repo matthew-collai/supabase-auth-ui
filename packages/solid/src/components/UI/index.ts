ba5ddce520dc41c87a1ad9f810a8f38c9c8d3bf9
Matthew <mwr645@gmail.com>
2023-07-05 16:48:58 -0500

    remove unused modules and update package

--- packages/solid/src/components/UI/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Anchor'
-export * from './Button'
-export * from './Container'
-export * from './Divider'
-export * from './Input'
-export * from './Label'
-export * from './Message'
